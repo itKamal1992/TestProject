e3da768c1c2556051a78147099bdc560721c62f2
KAMAL <kamal.h@adccacademy.com>
2019-09-30 16:23:33 +0530

    we2

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var T1="t1"
+        var T1="t1w"
     }
 }
